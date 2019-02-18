e83ee4aac7d9f1db5eb58eb2cc9b1da4fc9ad199
Zachary Zwierko <zzwierko@mail.umw.edu>
2019-02-18 18:41:09 -0500

    *** Initial commit. *** - Set up project skeleton. - Added project dependencies.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/***************************************************************************************************
+ * I hereby declare upon my word of honor that I have neither given nor received unauthorized help
+ * on this work.
+ *
+ * Zachary Zwierko
+ * HONR 491
+ * Spring 2019
+ **************************************************************************************************/
+
+"use strict";
+
+new Vue({
+    el: "main",
+    data: {
+
+    },
+    methods: {
+
+    }
+});
